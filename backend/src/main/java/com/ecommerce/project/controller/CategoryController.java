0fc9a92b4bd65db275c37353c815cd27f4b8079d
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-08 07:43:25 +0530

    feat: add validation
    Added custom exception APIException and ResourceNotFoundException.
    Used custom exception in CategoryServiceImpl.
    Created new query findByCategoryName in CategoryRepository.
    Removed exception being thrown from CategoryController as handled centrally by MyGlobalExceptionHandler.

--- backend/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ backend/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -45,22 +45,14 @@ public class CategoryController {
     
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-        try {
-            String response = categoryService.deleteCategory(categoryId);
-            return ResponseEntity.status(HttpStatus.OK).body(response);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
-    }
+        String response = categoryService.deleteCategory(categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+}
 
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> putMethodName(@RequestBody Category category, @PathVariable Long categoryId) {
-        try {
-            Category savedCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity<String> putMethodName(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+        Category savedCategory = categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
     }
 
 }
--- backend/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ backend/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -24,4 +24,14 @@ public class MyGlobalExceptionHandler {
 
         return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> myAPIException(APIException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
