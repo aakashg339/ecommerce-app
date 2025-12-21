c623adc02fda5fa112620dc3b67b050e827fb052
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-21 21:19:14 +0530

    refactor: Moved files saving logic to FileService
    Created a new FileService inferface and its implementation.
    Use FileServiceImpl to have code related to dealing with files.

--- backend/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ backend/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,14 +1,11 @@
 package com.ecommerce.project.service;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,6 +28,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
         Category category = categoryRepository.findById(categoryId)
@@ -123,7 +126,7 @@ public class ProductServiceImpl implements ProductService {
         // Upload image to server (saving in the /images folder)
         // Get the file name of uploaded image
         String path = "images/";
-        String filename = uploadImage(path, image);
+        String filename = fileService.uploadImage(path, image);
 
         // Updating the new file name to the product
         productFromDB.setImage(filename);
@@ -135,28 +138,6 @@ public class ProductServiceImpl implements ProductService {
         ProductDTO updateProductDTO = modelMapper.map(updateProduct, ProductDTO.class);
         
         return updateProductDTO;
-    }
-
-    private String uploadImage(String path, MultipartFile file) throws IOException {
-        // File names of current / original file
-        String originalFileName = file.getOriginalFilename();
-
-        // Generate a unique file name
-        String randomId = UUID.randomUUID().toString();
-        // mat.jpg --> 1234 --> 1234.jpg
-        String filename = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String filePath = path + File.pathSeparator + filename;
-
-        // Check if path exists and create
-        File folder = new File(path);
-        if(!folder.exists())
-            folder.mkdir();
-
-        // Upload to server
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-        // returning filename
-        return filename;
     }
 
     @Override
