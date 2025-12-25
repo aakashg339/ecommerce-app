bd917eed48ebf4579f88428485742dd0cbcc059f
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-25 15:27:05 +0530

    feat: Added relationship between user and roles
    Added many to many relation in user between user and roles

--- backend/src/main/java/com/ecommerce/project/model/User.java
+++ backend/src/main/java/com/ecommerce/project/model/User.java
@@ -1,15 +1,25 @@
 package com.ecommerce.project.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
@@ -40,5 +50,18 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @Getter
+    @Setter
+    @ManyToMany(
+        cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+        fetch = FetchType.EAGER
+    )
+    @JoinTable(
+        name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 
 }
