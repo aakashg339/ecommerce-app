5e522c7e1570b8a44113b421b877c31401a8ad08
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-25 16:13:47 +0530

    feat: Added relationships between user, address and product
    Added many to many relationship between user and address.
    Added one to many relatiuonship between user and product.

--- backend/src/main/java/com/ecommerce/project/model/User.java
+++ backend/src/main/java/com/ecommerce/project/model/User.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -13,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
@@ -75,5 +78,20 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+        name = "user_address",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
+    private List<Address> addresses = new ArrayList<>();
+
+    @OneToMany(
+        mappedBy = "user",
+        cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+        orphanRemoval = true
+    )
+    private Set<Product> products;
 
 }
