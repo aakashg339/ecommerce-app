0c087bcb9c4222d784cbe26df4792666c95391e5
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-28 16:11:07 +0530

    fix: unable to execute delete query
    Delete query requires modifying and transactional annotation.

--- backend/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ backend/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cardId, Long productId);
 
+    @Modifying
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
