880cfa835d36a128ad6fff00010a6159abba665f
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-04 10:39:54 +0530

    chore(frontend): Added required dependencies
    Added dependencies for materialui, react-icons and react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import { FaBeer } from 'react-icons/fa'
 import './App.css'
 
 function App() {
 
   return (
     <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-      Welcome
+      Welcome <FaBeer />
     </h1>
   )
 }
