49d82ee59beb5fddd64afd4a62d90083e48dc328
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-26 07:08:33 +0530

    fix(backend,frontend): Allowing configuring of URL using properties and changed the url from frontend
    frontend - Changed backend url port to be 5000.
    backend - frontend url is now taken from application.properties file.

--- backend/src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ backend/src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .cors(cors -> {})
             .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
             .sessionManagement(session
                 -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
