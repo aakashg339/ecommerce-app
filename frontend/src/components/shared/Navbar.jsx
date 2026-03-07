8d51481179321864febea3abe12675e1b0cf6d68
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-03-07 11:21:42 +0530

    feat(frontend): Updating NavBar on authentication

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
     const { cart } = useSelector((state) => state.carts);
+    const { user } = useSelector((state) => state.auth);
 
     return (
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -73,18 +74,24 @@ const Navbar = () => {
                                 </Badge>
                         </Link> 
                     </li>
-
-                    <li className="font-medium transition-all duration-150">
-                        <Link className="flex items-center space-x-2 px-4 py-[6px] 
-                                    bg-linear-to-r from-purple-600 to-red-500 
-                                    text-white font-semibold rounded-md shadow-lg 
-                                    hover:from-purple-500 hover:to-red-400 transition 
-                                    duration-300 ease-in-out transform "
-                            to="/login">
-                                <FaSignInAlt />
-                                <span>Login</span>
-                        </Link> 
-                    </li>
+                    
+                    { user && user.id ? (
+                        <li className="font-medium transition-all duration-150">
+                            <p>Welcome</p>
+                        </li>
+                    ) : (
+                        <li className="font-medium transition-all duration-150">
+                            <Link className="flex items-center space-x-2 px-4 py-[6px] 
+                                        bg-linear-to-r from-purple-600 to-red-500 
+                                        text-white font-semibold rounded-md shadow-lg 
+                                        hover:from-purple-500 hover:to-red-400 transition 
+                                        duration-300 ease-in-out transform "
+                                to="/login">
+                                    <FaSignInAlt />
+                                    <span>Login</span>
+                            </Link> 
+                        </li>
+                    ) }
                 </ul>
 
                 <button
