5cd1aa192f70c64e9dc45639680fb12efb21da1e
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-03-07 10:31:18 +0530

    feat(frontend): Added login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Contact from './components/Contact'
 import { Toaster } from 'react-hot-toast'
 import React from 'react'
 import Cart from './components/cart/Cart'
+import LogIn from './components/auth/Login'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
           <Route path='/about' element={ <About /> } />
           <Route path='/contact' element={ <Contact /> } />
           <Route path='/cart' element={ <Cart /> } />
+          <Route path='/login' element={ <LogIn /> } />
         </Routes>
       </Router>
       <Toaster position='bottom-center' />
