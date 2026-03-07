86027a1857077c85f70619ccfd39253914ed4fdb
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-03-07 14:45:21 +0530

    feat(frontend): Implementing private routes for authentication

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+const PrivateRoute = ({ publicPage = false }) => {
+    const { user } = useSelector((state) => state.auth);
+    
+    if (publicPage) {
+        return user ? <Navigate to="/profile" /> : <Outlet />
+    }
+    
+    return user ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default PrivateRoute
