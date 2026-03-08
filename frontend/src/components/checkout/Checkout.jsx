3347abc78edea1cf4de61c38b93cb25a8db96987
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-03-08 11:41:02 +0530

    feat(frontend): Setting up address selection layout

--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import { Step, StepLabel, Stepper } from '@mui/material';
 import React, { useState } from 'react'
+import AddressInfo from './AddressInfo';
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
@@ -24,6 +25,10 @@ const Checkout = () => {
                     ))
                 }
             </Stepper>
+
+            <div className='mt-5'>
+                {activeStep === 0 && <AddressInfo />}
+            </div>
         </div>
     )
 }
