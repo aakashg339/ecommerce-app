d0e6cd70f86c807648b6f314976eebf8c89b1c2f
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-03-10 07:28:49 +0530

    feat(frontend): Added layout to display error messages

--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUserAddresses } from '../../store/actions';
 import toast from 'react-hot-toast';
 import Skeleton from '../shared/Skeleton';
+import ErrorPage from '../shared/ErrorPage';
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
@@ -99,6 +100,8 @@ const Checkout = () => {
                     </button>
                 )} 
             </div>
+
+            { errorMessage && <ErrorPage message={errorMessage} /> }
         </div>
     )
 }
